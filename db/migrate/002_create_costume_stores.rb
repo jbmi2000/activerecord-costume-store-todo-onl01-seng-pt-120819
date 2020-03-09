417488592c4bde7968a084d3207fe5374a68e1b5
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-09 04:06:13 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :costume_store do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees_count
+      t.boolean :in_business
+      t.time :opening_time
+      t.time :closing_time
+    end
+  end
+end
+      
